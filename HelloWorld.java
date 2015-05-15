8dbe5e7877f65730571a7b82d494d9dd4bb1d542
hallt4 <hal12062@byui.edu>
2015-05-15 17:10:39 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 //This will be a hello world file
 class HelloWorld{
-
+//and this is mine
       public static void main(String [] args){
             System.out.println("Hello funkydunky worldy World!!");
             System.out.println("Watch your language!");
