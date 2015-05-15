57f3f67efba29a7efe14d90dcda48f0aacdd884c
Benjamin Patch <pat10005@byui.edu>
2015-05-15 18:01:03 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,8 @@
 class HelloWorld{
 
       public static void main(String [] args){
-<<<<<<< HEAD
-      System.out.println("Hello funkydunky worldy World!!");
-=======
-      System.out.println("Hello Dang World!!");
->>>>>>> origin/master
-      System.out.println("Watch your language!");
+            System.out.println("Hello funkydunky worldy World!!");
+            System.out.println("Watch your language!");
 }
 
 }
