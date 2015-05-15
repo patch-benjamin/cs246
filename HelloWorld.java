ac96713487d3f0483fe9901e53eafcdd5433a424
Benjamin Patch <benjaminarthurpatch@gmail.com>
2015-05-15 17:55:16 -0500

    language change

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 class HelloWorld{
 
       public static void main(String [] args){
-      System.out.println("Hello funk World!!");
+      System.out.println("Hello Dang World!!");
       System.out.println("Watch your language!");
 }
 
