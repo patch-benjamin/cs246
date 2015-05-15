7314d145ecbfada654177827a1a25ee91a0097a6
hallt4 <marilynwhittington@THMac.local>
2015-05-15 16:55:40 -0600

    T!
    n

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 class HelloWorld{
 
       public static void main(String [] args){
-      System.out.println("Hello funk World!!");
+      System.out.println("Hello funkydunky worldy World!!");
       System.out.println("Watch your language!");
 }
 
